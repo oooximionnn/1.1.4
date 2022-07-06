7b741d00be68d675c6114d2c3bcf14416759694b
oooximionnn <anton.e4maew@gmail.com>
2022-07-06 14:56:06 +0500

    Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,30 +1,35 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import java.sql.*;
 
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-        User user1 = new User("Viktor", "Petrov", (byte) 23);
-        User user2 = new User("Maria", "Sidorova", (byte) 55);
-        User user3 = new User("Elena", "Nikolaevna", (byte) 31);
-        User user4 = new User("Nikita", "Andreev", (byte) 19);
+        SessionFactory factory = new Configuration()
+            .configure("hibernate.cfg.xml")
+            .addAnnotatedClass(User.class)
+            .buildSessionFactory();
+        try {
+            Session session = factory.getCurrentSession();
+            User user1 = new User("Viktor", "Petrov", (byte) 23);
+            User user2 = new User("Maria", "Sidorova", (byte) 55);
+            User user3 = new User("Elena", "Nikolaevna", (byte) 31);
+            User user4 = new User("Nikita", "Andreev", (byte) 19);
 
-        UserDao userDao = new UserDaoJDBCImpl();
+            session.beginTransaction();
+            session.save(user1);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
 
-        userDao.createUsersTable();
-        userDao.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
-        userDao.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
-        userDao.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
-        userDao.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
-
-        System.out.println(userDao.getAllUsers());
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
     }
 }
